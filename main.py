56a67198f2b7b0f0dc4a8483bd61b8671fb0e318
treetree226 <tthai2025@kehillahstudent.org>
2024-10-23 10:40:45 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import turtle
 
 # YOUR CODE HERE
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
 
+turtle.left(225)
+turtle.forward(71)
+turtle.left(90)
+turtle.forward(71)
+turtle.left(90)
+turtle.forward(71)
+turtle.left(90)
+turtle.forward(71)
 turtle.exitonclick()
